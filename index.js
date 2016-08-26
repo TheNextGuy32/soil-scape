03c0457a94b5aaac5f43bd61b85195af91def609
Oliver <oliverbarnum32@gmail.com>
2016-08-26 18:51:45 -0400

    Move simulation code into submodule

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.utility = require('./lib/utility.js');
+exports.manager = require('./lib/manager.js');
+exports.requests = require('./lib/request.js');
+exports.renderer = require('./lib/render.js');
