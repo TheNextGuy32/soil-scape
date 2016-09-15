ebe247217ab1b76b08d88e4e24dcb267d5ac117d
Oliver <oliverbarnum32@gmail.com>
2016-09-15 17:32:53 -0400

    Expose useful util function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 const simulator = require('./lib/simulate.js');
+const utility = require('./lib/utility.js');
 
+exports.generateName = utility.generateName;
 exports.createSimulation = simulator.createSimulation;
-exports.toDays = require('./lib/utility.js').toDays;
+
+exports.toDays = utility.toDays;
 exports.simulate = simulator.simulateDays;
+
+exports.randomNumberBetween = utility.randomNumberBetween;
